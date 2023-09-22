805030108c4af44b756d9b68ae8ab983394114fa
V1k02 <103061125+V1k02@users.noreply.github.com>
2023-09-22 22:09:09 +0200

    4UFOspawnery

--- SpaceGame/Assets/Scripts/EnemyMovement.cs
+++ SpaceGame/Assets/Scripts/EnemyMovement.cs
@@ -4,6 +4,7 @@ public class EnemyMovement : MonoBehaviour
 {
     public Transform player;  // Reference to the player's ship
     public float moveSpeed = 5f;  // Adjust this value for the enemy's movement speed
+    public float raycastDistance = 2f;  // Distance to check for obstacles
 
     private void Start()
     {
@@ -23,7 +24,19 @@ public class EnemyMovement : MonoBehaviour
             // Calculate the direction towards the player
             Vector3 direction = (player.position - transform.position).normalized;
 
-            // Move the enemy towards the player
+            // Cast a ray forward to check for obstacles
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, raycastDistance))
+            {
+                // If an obstacle is detected, change direction randomly
+                Vector3 randomDirection = Random.insideUnitSphere;
+                randomDirection.y = 0;  // Ensure the direction is in the horizontal plane
+                direction += randomDirection.normalized;
+            }
+
+            // Move the enemy towards the new direction
             transform.Translate(direction * moveSpeed * Time.deltaTime);
         }
     }
