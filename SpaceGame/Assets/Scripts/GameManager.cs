cc85e055340cae232eee976c2856864bd06bb5cf
V1k02 <103061125+V1k02@users.noreply.github.com>
2023-09-23 15:27:47 +0200

    spawnuje sa lodka

---
+++ SpaceGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public SpawnPoint playerSpawnPoint;
+
+    
+
+    void Start()
+    {
+        // Consolidate all the logic to setup a scene inside a single method. 
+        // This makes it easier to call again in the future, in places other than the Start() method.
+        SetupScene();
+    }
+
+    public void SetupScene()
+    {
+        SpawnPlayer();
+    }
+
+    public void SpawnPlayer()
+    {
+        if (playerSpawnPoint != null)
+        {
+            GameObject player = playerSpawnPoint.SpawnObject();
+        }
+    }
+
+}
