abdc18faba4d5daecd6c6bbc16d86476d6372a0f
Marco147596 <seregmarco@gmail.com>
2023-09-24 21:41:57 +0200

    upravy

---
+++ SpaceGame/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 3;
+    public Rigidbody rb;
+    public GameObject deathEffect;
+    public int numOfHearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+    public float cooldown = 0.0000000000000000000000000000001f;
+
+    public Image[] hearts;
+
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Invoke("Die", 0.005f);
+        }
+    }
+
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+}
