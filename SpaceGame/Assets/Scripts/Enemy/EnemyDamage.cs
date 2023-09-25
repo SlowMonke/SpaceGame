0d732b5f8854ba8d79fb26f1c9ce6f3454cf74c7
V1k02 <103061125+V1k02@users.noreply.github.com>
2023-09-25 20:08:16 +0200

    dalsie zmeny

--- SpaceGame/Assets/Scripts/Enemy/EnemyDamage.cs
+++ SpaceGame/Assets/Scripts/Enemy/EnemyDamage.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +10,34 @@ public class EnemyDamage : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
+        PlayerHealth Player = hitInfo.GetComponent<PlayerHealth>();
+        if (Player != null)
+        {
+            Player.TakeDamage(1);
+
+        }
+        Destroy(gameObject);
+    }
+}*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public Transform player;
+    public Rigidbody2D rb;
+    public int damage = 1;
+    public CircleCollider2D circleCollider;
+
+    void start()
+    {
+        //Physics2D.IgnoreCollision(circleCollider, player.GetComponent<Collider2D>());
+    }
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+         Physics2D.IgnoreCollision(circleCollider, player.GetComponent<Collider2D>());
         PlayerHealth Player = hitInfo.GetComponent<PlayerHealth>();
         if (Player != null)
         {
