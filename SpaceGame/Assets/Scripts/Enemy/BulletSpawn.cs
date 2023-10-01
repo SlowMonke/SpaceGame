79b599ceaf4f4c767cc08d220c9bcf344fd49386
Marco147596 <seregmarco@gmail.com>
2023-10-01 13:03:30 +0200

    Revert "Revert "Pridal som fade medzi scenami""
    This reverts commit db0b956e3e305c3dcacda088adfa85b935aa7fcd.

---
+++ SpaceGame/Assets/Scripts/Enemy/BulletSpawn.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawn : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float ShootingDelay = 2f;
+
+    void Start()
+    {
+        ShootingDelay = 0;
+    }
+
+    void Update()
+    {
+        if (!PauseMenu.isPaused)
+        {
+            ShootingDelay -= Time.deltaTime;
+            
+                if (ShootingDelay <= 0)
+                {
+                    Shoot();
+                    ShootingDelay = 2f;
+                }
+        }
+
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
